f49d0b56c4a6072a1efa0cef8e916a61c73dd394
Atihh <65041022+Atihh@users.noreply.github.com>
2026-03-10 08:54:00 +0100

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify(req.body)
     });
+
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (err) {
